bb5f984c27550d587047cdca0c8ab9ca7f99faf2
871192i <65648092+871192i@users.noreply.github.com>
2024-10-26 03:41:48 +0000

    Update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+@st.cache_resource(show_spinner=False)
+def load_model():
+    # โค้ดโหลดโมเดลหรือทรัพยากร
+    return "โมเดลโหลดสำเร็จ"
+
+# ฟังก์ชันสำหรับล้างแคช
+def clear_cache():
+    st.cache_data.clear()  # ล้างแคชที่มาจาก @st.cache_data
+    st.cache_resource.clear()  # ล้างแคชที่มาจาก @st.cache_resource
+
+if st.button("Clear Cache"):
+    clear_cache()
+    st.success("Cache cleared!")
+
+st.title("🎈 My jekkiss a sdfasdfas app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
